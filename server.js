42299c455cf02ae34d104c2ca07a1ce284857c98
thevorgx <kh.elallali@gmail.com>
2024-10-02 22:27:38 +0100

    test

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+import express from 'express';
+import http from 'http';
+import routes from './routes/index';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/api', routes);
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
