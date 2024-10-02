2ccaddb90386ec586afc2b6eaddd5bc7496e64cf
thevorgx <kh.elallali@gmail.com>
2024-10-02 22:18:53 +0100

    task 1

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,47 @@
-#!/usr/bin/node
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    this.uri = `mongodb://${host}:${port}`;
+    this.dbClient = new MongoClient(this.uri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    this.dbClient.connect()
+      .then(() => {
+        this.db = this.dbClient.db(database);
+        console.log('MongoDB connected');
+      })
+      .catch((err) => {
+        console.error(`MongoDB connection error: ${err}`);
+      });
+  }
+
+  isAlive() {
+    return this.dbClient.topology && this.dbClient.topology.isConnected();
+  }
+
+  async nbUsers() {
+    if (!this.db) {
+      return 0;
+    }
+    const usersCollection = this.db.collection('users');
+    return usersCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.db) {
+      return 0;
+    }
+    const filesCollection = this.db.collection('files');
+    return filesCollection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
